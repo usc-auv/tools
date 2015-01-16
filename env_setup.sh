98bf12b3891fb1ae25b0e8de790ff5c6f1f37fab
Vinnie Magro <vmagro@socaldevs.com>
2015-01-15 18:00:41 -0800

    Install/setup scripts

---
+++ env_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+DIR=$(pwd)
+alias auv-repo="python $DIR/auv-repo.py"
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+BASEDIR=$(pwd)
+echo "Installing AUV tools to the current directory: $BASEDIR"
+echo "If you want to uninstall or move the tools, find the entry sourcing env_setup.sh in .bash_rc or .zsh_rc in this directory and remove it."
+
+if [ -f ~/.bash_profile ];
+then
+   echo "Adding entry to ~/.bash_profile"
+   echo "source $BASEDIR/env_setup.sh" >> ~/.bash_profile
+fi
+
+if [ -f ~/.bashrc ];
+then
+   echo "Adding entry to ~/.bashrc"
+   echo "source $BASEDIR/env_setup.sh" >> ~/.bashrc
+fi
+
+if [ -f ~/.zshrc ];
+then
+   echo "Adding entry to ~/.zshrc"
+   echo "source $BASEDIR/env_setup.sh" >> ~/.zshrc
+fi
