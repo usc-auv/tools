738401bb8f30506472eb8bf2a46f649cf8465857
Vinnie Magro <vmagro@socaldevs.com>
2015-01-15 17:30:23 -0800

    Status command

--- auv-repo.py
+++ auv-repo.py
@@ -92,9 +92,30 @@ def push():
                 click.echo("[" + project["name"] + "] ==> already up-to-date.")
 
 
+@click.command()
+def status():
+    with open('.manifest/manifest.json') as json_file:
+        data = json.load(json_file)
+        for project in data['projects']:
+            repo = Repo(project['path'])
+            commits_ahead = repo.iter_commits(repo.active_branch.name + '..origin/' + repo.active_branch.name)
+            commits_behind = repo.iter_commits('origin/' + repo.active_branch.name + '..' + repo.active_branch.name)
+            ahead_count = sum(1 for c in commits_ahead)
+            behind_count = sum(1 for c in commits_behind)
+            if ahead_count > 0 and behind_count == 0:
+                click.echo("[" + project["name"] + "] ==> ahead by " + ahead_count + " commits")
+            if ahead_count == 0 and behind_count > 0:
+                click.echo("[" + project["name"] + "] ==> behind by " + behind_count + " commits.")
+            if repo.is_dirty() or len(repo.untracked_files) != 0:
+                click.echo("[" + project["name"] + "] ==> there are unstaged changes or untracked files.")
+            elif not repo.is_dirty():
+                click.echo("[" + project["name"] + "] ==> no changes to commit.")
+
+
 cli.add_command(init)
 cli.add_command(sync)
 cli.add_command(push)
+cli.add_command(status)
 
 if __name__ == '__main__':
     cli()
